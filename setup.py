3fdd16a382e64cffed81b0cd22c8760dc44b51b0
Anto Idicherian Lonappan <antolonappan@icloud.com>
2023-02-13 01:37:39 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+files = ["cmblens/*"]
+setup(
+        name = 'cmblens',
+        packages = ['cmblens'],
+        package_data = {'cmblens' : files },
+        version = '1.1',
+        description = 'Lensing CMB with websky kappa',
+        author = 'Anto Idicherian Lonappan',
+        author_email = 'mail@antolonappan.me',
+        url = 'https://github.com/antolonappan/cmblens',
+        install_requires=['numpy','healpy','matplotlib','lenspyx','sqlalchemy','mpi4py']
+        )
